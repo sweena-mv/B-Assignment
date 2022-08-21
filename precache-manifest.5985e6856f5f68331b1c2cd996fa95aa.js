63dde10b41cdd7617257845a750431830da85b67
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2022-08-21 22:46:20 +0400

    Updates

---
+++ precache-manifest.5985e6856f5f68331b1c2cd996fa95aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2666bf306d6b4428cad",
+    "url": "/sweena-mv/B-Assignment.git/static/css/main.c6474adf.chunk.css"
+  },
+  {
+    "revision": "a2666bf306d6b4428cad",
+    "url": "/sweena-mv/B-Assignment.git/static/js/main.a2666bf3.chunk.js"
+  },
+  {
+    "revision": "e5fed62f2796369ce831",
+    "url": "/sweena-mv/B-Assignment.git/static/js/1.e5fed62f.chunk.js"
+  },
+  {
+    "revision": "b0857316d9c36b228138",
+    "url": "/sweena-mv/B-Assignment.git/static/css/2.d99fde76.chunk.css"
+  },
+  {
+    "revision": "b0857316d9c36b228138",
+    "url": "/sweena-mv/B-Assignment.git/static/js/2.b0857316.chunk.js"
+  },
+  {
+    "revision": "32fa24688c1806e9bbb4",
+    "url": "/sweena-mv/B-Assignment.git/static/js/runtime~main.32fa2468.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sweena-mv/B-Assignment.git/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4d4dc5e2645893963731d14af605f059",
+    "url": "/sweena-mv/B-Assignment.git/index.html"
+  }
+];
