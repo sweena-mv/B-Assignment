fe15916285cf3318f77aed336a181c0cf2fa9016
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2022-08-22 07:05:18 +0400

    Updates

---
+++ precache-manifest.5e2470e236f282bcfa3007c06d022d07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9519a75ce576695b9953",
+    "url": "/MyAssignment3/static/css/main.8f1c39ad.chunk.css"
+  },
+  {
+    "revision": "9519a75ce576695b9953",
+    "url": "/MyAssignment3/static/js/main.9519a75c.chunk.js"
+  },
+  {
+    "revision": "e5fed62f2796369ce831",
+    "url": "/MyAssignment3/static/js/1.e5fed62f.chunk.js"
+  },
+  {
+    "revision": "b0857316d9c36b228138",
+    "url": "/MyAssignment3/static/css/2.d99fde76.chunk.css"
+  },
+  {
+    "revision": "b0857316d9c36b228138",
+    "url": "/MyAssignment3/static/js/2.b0857316.chunk.js"
+  },
+  {
+    "revision": "92f7243119b24cc144dd",
+    "url": "/MyAssignment3/static/js/runtime~main.92f72431.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/MyAssignment3/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9d8c7d9688630d5df00aa1620ba42130",
+    "url": "/MyAssignment3/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyAssignment2/precache-manifest.7b8a722da8e75a141d576b8cca92dc13.js"
+  "/MyAssignment3/precache-manifest.5e2470e236f282bcfa3007c06d022d07.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MyAssignment2/index.html", {
+workbox.routing.registerNavigationRoute("/MyAssignment3/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
